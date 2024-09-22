357e0d73070d9f9401c72164609b22c6da221ff7
Neil Morrison <email@neil.today>
2024-09-22 02:51:44 +0100

    Still skipping a byte...

--- src/bits_bytes_iter/mod.rs
+++ src/bits_bytes_iter/mod.rs
@@ -1,10 +1,21 @@
-pub struct OffsetReader<I: Iterator<Item = u8>> {
+use std::fmt::Debug;
+
+pub struct BitsBytesIter<'a, I: Iterator<Item = &'a u8>> {
     bit_offset: u8,
     raw_iter: I,
-    window: [Option<u8>; 2],
+    window: [Option<&'a u8>; 2],
 }
 
-impl<I: Iterator<Item = u8>> Iterator for OffsetReader<I> {
+impl<'a, I: Iterator<Item = &'a u8>> Debug for BitsBytesIter<'a, I> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BitsBytesIter")
+            .field("bit_offset", &self.bit_offset)
+            .field("window", &self.window)
+            .finish()
+    }
+}
+
+impl<'a, I: Iterator<Item = &'a u8>> Iterator for BitsBytesIter<'a, I> {
     type Item = u8;
     fn next(&mut self) -> Option<u8> {
         let byte = self.byte_from_window();
@@ -13,7 +24,7 @@ impl<I: Iterator<Item = u8>> Iterator for OffsetReader<I> {
     }
 }
 
-impl<I: Iterator<Item = u8>> OffsetReader<I> {
+impl<'a, I: Iterator<Item = &'a u8>> BitsBytesIter<'a, I> {
     pub fn new(mut raw_iter: I) -> Self {
         let window = [raw_iter.next(), raw_iter.next()];
         Self {
@@ -24,9 +35,7 @@ impl<I: Iterator<Item = u8>> OffsetReader<I> {
     }
 
     pub fn next_bit(&mut self) -> Option<bool> {
-        let upcoming_byte = self.byte_from_window();
-
-        match upcoming_byte {
+        match self.byte_from_window() {
             Some(byte) => {
                 self.advance_bit_offset();
                 Some((byte & 0b10000000) != 0)
@@ -38,6 +47,7 @@ impl<I: Iterator<Item = u8>> OffsetReader<I> {
     fn advance_bit_offset(&mut self) -> () {
         self.bit_offset += 1;
         if self.bit_offset == 8 {
+            println!("bit offset reset");
             self.bit_offset = 0;
             self.load_next_from_raw();
         }
@@ -55,7 +65,7 @@ impl<I: Iterator<Item = u8>> OffsetReader<I> {
             [None, Some(_)] => todo!(),
             [Some(current_byte), None] => {
                 if self.bit_offset == 0 {
-                    Some(current_byte)
+                    Some(*current_byte)
                 } else {
                     let first_part = current_byte << self.bit_offset;
                     Some(first_part)
@@ -63,7 +73,7 @@ impl<I: Iterator<Item = u8>> OffsetReader<I> {
             }
             [Some(current_byte), Some(next_byte)] => {
                 if self.bit_offset == 0 {
-                    return Some(current_byte);
+                    return Some(*current_byte);
                 }
 
                 let first_part = current_byte << self.bit_offset;
@@ -76,7 +86,7 @@ impl<I: Iterator<Item = u8>> OffsetReader<I> {
 
 #[cfg(test)]
 mod tests {
-    use crate::offset_reader;
+    use crate::bits_bytes_iter;
 
     use super::*;
 
@@ -85,9 +95,9 @@ mod tests {
         let first: u8 = 0b10101010;
         let second: u8 = 0b11001100;
 
-        let input = vec![first, second];
+        let input = vec![&first, &second];
 
-        let mut or = OffsetReader::new(input.into_iter());
+        let mut or = BitsBytesIter::new(input.into_iter());
 
         let bits = [
             or.next_bit(),
@@ -137,9 +147,8 @@ mod tests {
         let first: u8 = 0b10101010;
         let second: u8 = 0b11001100;
 
-        let input = vec![first, second];
-
-        let mut or = OffsetReader::new(input.into_iter());
+        let input = vec![&first, &second];
+        let mut or = BitsBytesIter::new(input.into_iter());
 
         let bytes = [or.next(), or.next(), or.next()];
         assert_eq!(bytes, [Some(0b10101010), Some(0b11001100), None]);
@@ -150,8 +159,8 @@ mod tests {
         let first: u8 = 0b10101010;
         let second: u8 = 0b11001100;
 
-        let input = vec![first, second];
-        let mut or = OffsetReader::new(input.into_iter());
+        let input = vec![&first, &second];
+        let mut or = BitsBytesIter::new(input.into_iter());
 
         let bit = or.next_bit();
         assert_eq!(bit, Some(true));
@@ -165,8 +174,8 @@ mod tests {
         let first: u8 = 0b10101010;
         let second: u8 = 0b11001100;
 
-        let input = vec![first, second];
-        let mut or = OffsetReader::new(input.into_iter());
+        let input = vec![&first, &second];
+        let mut or = BitsBytesIter::new(input.into_iter());
 
         let bits: [Option<bool>; 4] = [or.next_bit(), or.next_bit(), or.next_bit(), or.next_bit()];
         assert_eq!(bits, [Some(true), Some(false), Some(true), Some(false)]);
@@ -174,5 +183,50 @@ mod tests {
         let result = [or.next(), or.next(), or.next()];
 
         assert_eq!(result, [Some(0b10101100), Some(0b11000000), None]);
+    }
+
+    #[test]
+    fn big_file() {
+        let input = include_bytes!("../../tsz-compressed-data.bin");
+
+        let mut or = BitsBytesIter::new(input.into_iter());
+
+        let result: Vec<u8> = or.collect();
+
+        let expected_output = include_bytes!("../../tsz-compressed-data.bin");
+        assert_eq!(result, expected_output);
+    }
+
+    #[test]
+    fn big_file_bit_shifted() {
+        let input = include_bytes!("../../tsz-compressed-data.bin");
+        let mut or = BitsBytesIter::new(input.into_iter());
+
+        or.next();
+        for _ in 0..3 {
+            or.next_bit();
+        }
+
+        or.next();
+
+        for _ in 0..5 {
+            or.next_bit();
+        }
+
+        or.next();
+
+        let result: Vec<u8> = or.collect();
+
+        let expected_output = include_bytes!("../../tsz-compressed-data.bin");
+        let mut expected_output_iter = expected_output.into_iter();
+
+        expected_output_iter.next();
+        expected_output_iter.next();
+        expected_output_iter.next();
+        expected_output_iter.next();
+
+        let modified_expected_output: Vec<u8> = expected_output_iter.map(|v| *v).collect();
+
+        assert_eq!(result, modified_expected_output);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(gen_blocks)]
 
+use bits_bytes_iter::BitsBytesIter;
 use core::panic;
-mod offset_reader;
+
+mod bits_bytes_iter;
 
 pub struct HeatShrink<const W: usize, const L: usize> {
     window: [u8; W],
@@ -10,7 +12,7 @@ pub struct HeatShrink<const W: usize, const L: usize> {
     lookahead_index: usize,
 }
 
-impl<const W: usize, const L: usize> HeatShrink<W, L> {
+impl<'a, const W: usize, const L: usize> HeatShrink<W, L> {
     // Constructor: initialize the window and lookahead buffers to zero
     pub fn new() -> Self {
         Self {
@@ -21,52 +23,98 @@ impl<const W: usize, const L: usize> HeatShrink<W, L> {
         }
     }
 
-    pub fn decode<I: Iterator<Item = u8>>(&mut self, mut input: I) -> impl Iterator<Item = u8> {
+    pub fn decode<I: Iterator<Item = &'a u8>>(&mut self, mut input: I) -> impl Iterator<Item = u8> {
         gen move {
-            let mut or = offset_reader::OffsetReader::new(input);
+            let mut bb_iter = BitsBytesIter::new(input);
 
-            let has_next = true;
-
-            while has_next {
-                let bit = or.next_bit();
-
+            let mut loop_count: usize = 0;
+            while let Some(bit) = bb_iter.next_bit() {
                 match bit {
-                    Some(true) => {
-                        if let Some(byte) = or.next() {
+                    true => {
+                        dbg!(&bb_iter);
+
+                        if let Some(byte) = bb_iter.next() {
+                            // dbg!(byte);
+                            if byte == 186 {
+                                println!("---- WRONG VALUE ----");
+                                dbg!(&bb_iter);
+                            }
                             yield self.prep_output_byte(byte);
                         } else {
                             return;
                         }
                     }
-                    Some(false) => {
-                        let msb_index_byte: usize = or.next().unwrap().into();
-                        let lsb_index_byte: usize = or.next().unwrap().into();
+                    false => {
+                        let back_ref_index = if W > 255 {
+                            let msb_index_byte = bb_iter.next();
+                            let lsb_index_byte = bb_iter.next();
 
-                        let back_ref_index: usize = (msb_index_byte << 8) | lsb_index_byte;
+                            if let [Some(msb_index_byte), Some(lsb_index_byte)] =
+                                [msb_index_byte, lsb_index_byte]
+                            {
+                                let msb_index_byte: usize = msb_index_byte.into();
+                                let lsb_index_byte: usize = lsb_index_byte.into();
+                                let back_ref_index: usize = (msb_index_byte << 8) | lsb_index_byte;
+                                back_ref_index
+                            } else {
+                                return;
+                            }
+                        } else {
+                            let back_ref_index = bb_iter.next();
 
-                        let msb_count_byte: usize = or.next().unwrap().into();
-                        let lsb_count_byte: usize = or.next().unwrap().into();
+                            if let Some(back_ref_index) = back_ref_index {
+                                back_ref_index.into()
+                            } else {
+                                return;
+                            }
+                        };
 
-                        let count: usize = (msb_count_byte << 8) | lsb_count_byte;
+                        let mut count_bits: [bool; L] = [false; L];
+
+                        for i in 0..L {
+                            let bit = bb_iter.next_bit();
+                            if let Some(bit) = bit {
+                                count_bits[i] = bit;
+                            } else {
+                                return;
+                            }
+                        }
+
+                        let count = self.read_number_from_bits(&mut count_bits) + 1;
 
                         for _ in 0..count {
                             // since we always add 1 to the window index when we output the back_ref_index doesn't need to change
                             let window_value = self.get_window_value(back_ref_index);
                             yield self.prep_output_byte(window_value);
                         }
-                    }
-                    None => {
-                        return;
+
+                        if loop_count > 1 {
+                            return;
+                        }
+                        loop_count += 1;
+                        // return;
                     }
                 }
             }
         }
     }
 
+    fn read_number_from_bits(&mut self, bits: &mut [bool; L]) -> usize {
+        let mut result = 0;
+
+        for (i, &bit) in bits.iter().enumerate() {
+            if bit {
+                result |= 1 << (L - 1 - i);
+            }
+        }
+
+        result
+    }
+
     fn prep_output_byte(&mut self, byte: u8) -> u8 {
         self.window[self.window_index] = byte;
         self.window_index += 1;
-        if self.window_index == W {
+        if self.window_index == (W - 1) {
             self.window_index = 0;
         }
 
@@ -74,28 +122,68 @@ impl<const W: usize, const L: usize> HeatShrink<W, L> {
     }
 
     fn get_window_value(&self, back_index: usize) -> u8 {
-        if self.window_index > back_index {
-            // no need to loop around
+        if self.window_index >= back_index {
             return self.window[self.window_index - back_index];
         } else {
-            let remainer = self.window_index - back_index;
-            return self.window[W - remainer];
+            let remainer = self.window_index % back_index;
+            return self.window[(W - 1) - remainer];
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{fs::File, io::Write};
+
     use super::*;
 
     #[test]
-    fn passthrough() {
-        let input: Vec<u8> = vec![0, 1, 2, 3];
+    fn passthrough_bytes() {
+        let first: u8 = 0b11000000;
+        let second: u8 = 0b11100000;
+        let input: Vec<&u8> = vec![&first, &second];
         let mut hs = HeatShrink::<13, 4>::new();
 
         let out = hs.decode(input.into_iter());
 
         let result: Vec<u8> = out.collect();
-        assert_eq!(result, vec![0, 1, 2, 3]);
+        assert_eq!(result, vec![129, 128]);
+    }
+
+    #[test]
+    fn compare_loop() {
+        let input = include_bytes!("../tsz-compressed-data.bin.hs");
+        let mut hs = HeatShrink::<256, 8>::new();
+
+        let input_iter = (*input).iter();
+
+        let mut out = hs.decode(input_iter);
+
+        let expected_output = include_bytes!("../tsz-compressed-data.bin");
+        let mut expected_iter = expected_output.iter();
+
+        while let Some(expected) = expected_iter.next() {
+            let actual = out.next().unwrap();
+
+            assert_eq!(actual, *expected);
+        }
+    }
+
+    #[test]
+    fn decode_bytes() {
+        let input = include_bytes!("../tsz-compressed-data.bin.hs");
+        let mut hs = HeatShrink::<256, 8>::new();
+
+        let input_iter = (*input).iter();
+
+        let out = hs.decode(input_iter);
+
+        let result: Vec<u8> = out.collect();
+
+        let mut file = File::create("./test-output.bin").unwrap();
+        file.write_all(&result).unwrap();
+
+        // let expected_output = include_bytes!("../tsz-compressed-data.bin");
+        // assert_eq!(result, expected_output);
     }
 }
